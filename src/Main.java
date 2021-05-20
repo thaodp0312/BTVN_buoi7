358f59aadde67ce1ba5c1cb8ee3b7d59b9fb5676
thaodp0312 <83717046+thaodp0312@users.noreply.github.com>
2021-05-20 08:58:34 +0700

    Delete src directory

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-//mangsonguyen msn = new mangsonguyen();
-//msn.mangnguyen();
-mangchuoi mc= new mangchuoi();
-mc.mangxau();
-    }
-}
